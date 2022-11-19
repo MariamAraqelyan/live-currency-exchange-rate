f0ae977f83e06fcaa81bb2c0b28b8cfe69ca376c
MarMariam <m.arakelyan@omedia.ge>
2022-11-19 22:41:45 +0100

    new

---
+++ src/app/modules/live-prices/live-prices.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LivePricesComponent } from './live-prices.component';
+
+describe('LivePricesComponent', () => {
+  let component: LivePricesComponent;
+  let fixture: ComponentFixture<LivePricesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LivePricesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LivePricesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/live-prices/live-prices.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LivePricesComponent } from './live-prices.component';
+
+
+
+@NgModule({
+  declarations: [
+    LivePricesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LivePricesModule { }
